70c19d63b7ccab06f5bcaf43aa44a3a60756e460
gtandu <tbrandon91@hotmail.fr>
2016-03-14 22:05:08 +0100

    Creation de la classe Document

---
+++ suivi_de_projet_miage/src/modele/Document.java
@@ -0,0 +1,29 @@
+package modele;
+
+import java.io.File;
+
+public class Document {
+	
+	private String nom;
+	
+	private File document;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public File getDocument() {
+		return document;
+	}
+
+	public void setDocument(File document) {
+		this.document = document;
+	}
+	
+	
+
+}
