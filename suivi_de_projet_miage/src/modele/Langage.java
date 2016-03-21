480f9908a7284c9f53e643c32ef3ab7113f46388
EL GOFF Saher <saher.elgoff@hotmail.fr>
2016-03-21 08:50:32 +0100

    MODIF parametre de constructeur
    Classe Groupe/Tache/Langage/MotCle.

--- suivi_de_projet_miage/src/modele/Langage.java
+++ suivi_de_projet_miage/src/modele/Langage.java
@@ -1,30 +1,20 @@
 package modele;
 
-
 public class Langage {
-	
 
-	private Long id;
-	
 	private String nom;
-	
-	public static long  ID = 0;
-	
-	public Langage(Long id, String nom) {
-		
-		this.id = ++ID;
+
+	public static long ID = 0;
+
+	public Langage(String nom) {
+
 		this.nom = nom;
 	}
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
+
 	public String getNom() {
 		return nom;
 	}
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
