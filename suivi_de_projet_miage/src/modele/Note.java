9c51757df09de43fc62708656c95496bd1af6f5e
EL GOFF Saher <saher.elgoff@hotmail.fr>
2016-04-18 11:05:33 +0200

    Classe Note
    Class note

---
+++ suivi_de_projet_miage/src/modele/Note.java
@@ -0,0 +1,29 @@
+package modele;
+
+public class Note {
+	
+	private String projet;
+	
+	private String groupe;
+
+	public String getProjet() {
+		return projet;
+	}
+
+	public void setProjet(String projet) {
+		this.projet = projet;
+	}
+
+	public String getGroupe() {
+		return groupe;
+	}
+
+	public void setGroupe(String groupe) {
+		this.groupe = groupe;
+	}
+	
+	
+	
+	
+
+}
